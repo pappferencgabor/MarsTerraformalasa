6bdf4117a4129c8f48fd19d92d8dc1595c9a3485
pappferencgabor <p.feco2005@gmail.com>
2024-10-16 13:10:25 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./app.css"
+
+function App() {
+	return (
+		<div className="container">
+			<div className="playerName">
+				<h1>Player Name</h1>
+			</div>
+
+			<div className="resourceContainer">
+				<p>Acél: <span>50</span></p>
+
+				<div className="buttonContainer">
+					<button type="button">-</button>
+					<button type="button">+</button>
+				</div>
+			</div>
+			<div className="resourceContainer">
+				<p>Acél: <span>50</span></p>
+
+				<div className="buttonContainer">
+					<button type="button">-</button>
+					<button type="button">+</button>
+				</div>
+			</div>
+			<div className="resourceContainer">
+				<p>Acél: <span>50</span></p>
+
+				<div className="buttonContainer">
+					<button type="button">-</button>
+					<button type="button">+</button>
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default App
